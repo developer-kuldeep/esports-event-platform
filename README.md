ac813f6e2e4cb8c624cf835bb37885b9160f2f82
kuldeep <kuldeephimachal83@gmail.com>
2024-09-06 00:23:34 +0530

    deleted all

--- README.md
+++
@@ -1,2 +0,0 @@
-# esports-event-platform
-A versatile platform for organizing and running esports events, featuring capabilities for tournament setup, player management, and event tracking.
--- core-app/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-// src/app/app.routes.ts
-import { RouterModule, Routes } from '@angular/router';
-
-export const routes: Routes = [
-];
-
-export const AppRoutingModule = RouterModule.forRoot(routes);
