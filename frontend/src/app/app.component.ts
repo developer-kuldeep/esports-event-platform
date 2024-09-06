b695a7e7e4b82e1d4883261485f0516c7a272564
kuldeep <kuldeephimachal83@gmail.com>
2024-09-07 01:58:41 +0530

    custom design

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { TournamentsComponent } from './components/tournaments/tournaments.component';
+import { TeamsComponent } from './components/teams/teams.component';
+import { LeaderboardComponent } from './components/leaderboard/leaderboard.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [
+    NavbarComponent,
+    HeroComponent,
+    TournamentsComponent,
+    TeamsComponent,
+    LeaderboardComponent,
+    ContactComponent,
+    FooterComponent
+  ]
 })
-export class AppComponent {
-  title = 'frontend';
-}
+export class AppComponent { }
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroComponent } from './components/hero/hero.component';
+import { TournamentsComponent } from './components/tournaments/tournaments.component';
+import { TeamsComponent } from './components/teams/teams.component';
+import { LeaderboardComponent } from './components/leaderboard/leaderboard.component';
+import { ContactComponent } from './components/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HeroComponent },
+  { path: 'tournaments', component: TournamentsComponent },
+  { path: 'teams', component: TeamsComponent },
+  { path: 'leaderboard', component: LeaderboardComponent },
+  { path: 'contact', component: ContactComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
